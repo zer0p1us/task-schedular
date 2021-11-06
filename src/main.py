2a997801355cb6189871c009eff8ff8412fa56ca
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-11-06 17:57:20 +0000

    added main script

---
+++ src/main.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name: main
+# Purpose: entry point of the program
+#
+# Author: LloydLudwig
+#
+# Created: 04/11/2021
+# Copyright: (c) LloydLudwig 2021
+# Licence: GPL 2.0
+#-------------------------------------------------------------------------------
+
+
+def main():
+    while True:
+        print("blob")
+
+    pass
+
+if __name__ == "__main__":
+    main()
