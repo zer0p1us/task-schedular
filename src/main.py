2dfbc4ca14c5af18cdc5f904a091ae0de24d40d4
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-11-23 11:38:15 +0000

    removed depricated comments

--- src/main.py
+++ src/main.py
@@ -1,15 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name: main
-# Purpose: entry point of the program
-#
-# Author: LloydLudwig
-#
-# Created: 04/11/2021
-# Copyright: (c) LloydLudwig 2021
-# Licence: GPL 2.0
-#-------------------------------------------------------------------------------
-
-
 def main():
     while True:
         print("blob")
