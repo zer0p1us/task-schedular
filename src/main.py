3e9d966ab06fc6f2d399e32565ec6272dc685726
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-11-23 12:32:00 +0000

    added basic test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-def main():
-    while True:
-        print("blob")
+from task import task
+import random
 
+def main():
+    task_1 = task(random.randint(0, 100), str(input("please enter task title: ")))
+    print(task_1.__str__())
+    task_2 = task(task_title=input("please enter task title: "))
     pass
 
 if __name__ == "__main__":
