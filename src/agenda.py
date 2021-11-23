b64ee23fb908008f33d5f4697350941d2955ee17
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-11-23 11:38:15 +0000

    removed depricated comments

--- src/agenda.py
+++ src/agenda.py
@@ -1,12 +1,1 @@
-#-------------------------------------------------------------------------------
-# Name: main
-# Purpose: entry point of the program
-#
-# Author: LloydLudwig
-#
-# Created: 04/11/2021
-# Copyright: (c) LloydLudwig 2021
-# Licence: GPL 2.0
-#-------------------------------------------------------------------------------
-
 class Agenda():
