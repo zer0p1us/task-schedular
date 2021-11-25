cebb27d29d5f65c9d6934808c2808bb4e4ef4f01
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-11-25 16:57:42 +0000

    implemented methods

--- src/agenda.py
+++ src/agenda.py
@@ -1,13 +1,23 @@
 from task import task
-class Agenda():
-    tasks = list(task)
+class agenda():
+    tasks = []
 
     def __init__(self, tasks=None):
-        self.tasks = tasks
+        if tasks == None:
+            self.tasks = list()
+        else:
+            self.tasks = tasks
 
-    def add_task(self, task):
-        tasks.append(task)
+    def add_task(self):
+        self.tasks.append(task(
+            input("please enter a task title: "),
+            input("please enter a task description: "),
+            input("please enter a task deadline (dd/mm/yyyy, hh:mm): "),
+            input("please enter a task urgency level (0-4), lower level is higher urgency: ")))
         pass
 
     def get_tasks(self):
-        return tasks
+        return self.tasks
+
+    def __str__(self):
+        return [for i in self.tasks : i.__str__())]
