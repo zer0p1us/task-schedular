afcf6c2e235997e2aa461c1b6d7d162e7825d167
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-11-25 09:13:12 +0000

    added random id

--- src/task.py
+++ src/task.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass, field
 
 @dataclass
-class task():
-	id: int
+class task(object):
+	id: int = field(random.randint(0, 1000))
 	task_title: str = field(default="none")
 	task_description: str = field(default="none")
 	deadline: str = field(default="none")
