9faa04de9be894ae1f0183ce7080ab87138b1b9c
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-11-25 09:10:56 +0000

    implemented class

--- src/agenda.py
+++ src/agenda.py
@@ -1,1 +1,13 @@
+from task import task
 class Agenda():
+    tasks = list(task)
+
+    def __init__(self, tasks=None):
+        self.tasks = tasks
+
+    def add_task(self, task):
+        tasks.append(task)
+        pass
+
+    def get_tasks(self):
+        return tasks
