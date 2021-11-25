ec6b96226e51cb6df11f0b7a424d4ff5dfaeb499
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-11-25 09:12:38 +0000

    added menu

--- src/main.py
+++ src/main.py
@@ -2,9 +2,28 @@ from task import task
 import random
 
 def main():
-    task_1 = task(random.randint(0, 100), str(input("please enter task title: ")))
-    print(task_1.__str__())
-    task_2 = task(task_title=input("please enter task title: "))
+    while True:
+        option = int(input("""
+        options:
+
+		0: Quit
+		1: View tasks
+		2: Add new entry to the schedule
+		3: Update entry
+        0"""))
+
+        match option:
+            case 0:
+                break
+            case 1:
+
+            case 2:
+                new_task = task()
+
+            case 3:
+
+            case _:
+                print(f"option {option} is not available, please chose from the available menu")
     pass
 
 if __name__ == "__main__":
