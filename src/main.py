c96f11c8282660a518f7e060563f441f33e7ad89
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-11-30 12:17:27 +0000

    added error handling for user input

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from types import NoneType
 from task import task
 from agenda import agenda
 import random
@@ -5,14 +6,27 @@ import random
 def main():
     _agenda = agenda()
     while True:
-        option = int(input("""
-        options:
 
-		0: Quit
-		1: View tasks
-		2: Add new entry to the schedule
-		3: Update entry
-        """))
+        # to handle user input erros
+        try:
+            option = input("""
+            options:
+
+            0: Quit
+            1: View tasks
+            2: Add new entry to the schedule
+            3: Update entry
+            """)
+            option = int(option) #do not touch, try&except needs option to initialise to sort error
+        except ValueError:
+            # input is empty
+            if not option:
+                print("Please select one of the options")
+            # input is not integer
+            else:
+                print("Please enter an interger option")
+            continue
+
 
         match option:
             case 0:
