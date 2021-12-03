e1c0541e243931e8c7e11e2fdff743d34677162c
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-12-03 18:43:29 +0000

    reverted to __str__

--- src/task.py
+++ src/task.py
@@ -10,9 +10,9 @@ class task(object):
 	urgency: int = field(default=0)
 
 
-	# def __str__(self):
-	# 	"""return task data"""
-	# 	return f"id: {self.id} title: {self.title} description: {self.description}, deadline: {self.deadline}"
-
 	def __str__(self):
+		"""return task data"""
+		return f"\nid: {self.id} \ntitle: {self.title} \ndescription: {self.description} \ndeadline: {self.deadline}"
+
+	def save_format(self):
 		return {"id" : self.id, "title": self.title, "description": self.description, "deadline": self.deadline}
