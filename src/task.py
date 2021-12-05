44b635f6d7315951f091903b3fd47cba9c0a96fd
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-12-05 13:17:27 +0000

    improved __str__ deadline is the date rather than timestamp progress is also added

--- src/task.py
+++ src/task.py
@@ -14,7 +14,7 @@ class task():
 
 	def __str__(self):
 		"""return task data"""
-		return f"\ntitle: {self.title} \ndescription: {self.description} \ndeadline: {self.deadline} \nurgency: {self.urgency} \nsmart urgency: {self.smart_urgency_calc()}"
+		return f"\ntitle: {self.title} \ndescription: {self.description} \ndeadline: {datetime.fromtimestamp(self.deadline)} \nurgency: {self.urgency} \nsmart urgency: {self.smart_urgency_calc()} \nprogress: {self.progress}"
 
 	def save_format(self):
 		"""return dict of attributes for save format"""
