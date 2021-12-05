64c3e5038f69d8eb88e946a314cfbfe639214bbe
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-12-05 13:32:31 +0000

    added try except for save_agenda function

--- src/agenda.py
+++ src/agenda.py
@@ -28,11 +28,14 @@ class agenda():
 		return [f.__str__() for f in self.tasks]
 
 	def save_agenda(self, file_path="agenda.csv"):
+		try:
 		with open(file_path, "w")as csv_file:
 			agenda_file = csv.DictWriter(csv_file, fieldnames=task.fields, delimiter=',', lineterminator='\r')
 			agenda_file.writeheader()
 			agenda_file.writerows([f.save_format() for f in self.tasks])
 			csv_file.close()
+		except:
+			print("could not write file for unknown reasons")
 
 	def load_agenda(self, file_name):
 		file = open(file_name)
