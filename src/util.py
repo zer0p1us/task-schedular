436327c5e6023701961f3f2903ccb7ce44552d81
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-12-05 20:03:12 +0000

    added try/except for parcing date input

--- src/util.py
+++ src/util.py
@@ -6,9 +6,13 @@ class util:
 	@staticmethod
 	def date_parcing() -> int:
 		"""take user input and return timestamp"""
-		raw_date = input("please enter the date (dd/mm/yyyy): ")
-		day, month, year = map(int, raw_date.split('/'))
-		raw_hour = input("please enter the hour (hh:mm): ")
+		try:
+			raw_date = input("please enter the date (dd/mm/yyyy): ")
+			day, month, year = map(int, raw_date.split('/'))
+			raw_hour = input("please enter the hour (hh:mm): ")
+		except ValueError:
+			print("Error: the date entered is invalid, the deadline will be provisionally set to current time, please update in duetime")
+			return datetime.now().timestamp()
 		if not raw_hour:
 			return int(datetime(year=year, month=month, day=day).timestamp())
 		else:
