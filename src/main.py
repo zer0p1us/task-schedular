3362176ba274523ea0c9bbd01765bfab9415ce67
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-12-10 19:25:47 +0000

    converted menu selection to match cases

--- src/main.py
+++ src/main.py
@@ -24,22 +24,22 @@ def main():
 				print("Please select an integer option")
 			continue
 
-
-		if option == 0:
-			_agenda.save_agenda()
-			break
-		elif option == 1:
-			_agenda.table() if len(_agenda.tasks) != 0 else print("no tasks present")
-		elif option == 2:
-			_agenda.add_task()
-		elif option == 3:
-			_agenda.table() if len(_agenda.tasks) != 0 else print("no tasks present")
-			try:
-				_agenda.update_task(int(input("please select the index of the task to update: ")))
-			except ValueError:
-				print("the index entered is invalid")
-		else:
-			print(f"option {option} is not available, please chose from the available menu")
+		match option:
+			case 0:
+				_agenda.save_agenda()
+				break
+			case 1:
+				_agenda.table() if len(_agenda.tasks) != 0 else print("no tasks present")
+			case 2:
+				_agenda.add_task()
+			case 3:
+				_agenda.table() if len(_agenda.tasks) != 0 else print("no tasks present")
+				try:
+					_agenda.update_task(int(input("please select the index of the task to update: ")))
+				except ValueError:
+					print("the index entered is invalid")
+			case _:
+				print(f"option {option} is not available, please chose from the available menu")
 	pass
 
 if __name__ == "__main__":
